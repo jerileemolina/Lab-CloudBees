8577de02a424f56cb4fdcb81d5703d6ffe0b4506
jerileemolina <j.molina.velasco@accenture.com>
2022-10-14 10:50:44 +0200

    Etapas añadidas

---
+++ Ej1.groovy
@@ -0,0 +1,16 @@
+pipeline {
+    agent any
+    stages { 
+        stage('Buzz Buzz') {
+            steps {
+		echo 'Bees Buzz!'
+        }
+    }
+        stage ('Bees bees') {
+            steps {
+        echo 'Buzz,Bees,Buzz!'
+        echo 'Bees Buzzing'
+       }
+     }
+   }
+}
