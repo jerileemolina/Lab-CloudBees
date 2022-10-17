3d9aea553f5ea26a459c92a7c7f97b81dbedf81f
jerileemolina <j.molina.velasco@accenture.com>
2022-10-17 10:40:35 +0200

    Added JUnit test results configuration

--- ej3.groovy
+++ ej3.groovy
@@ -4,14 +4,14 @@ pipeline {
     stage('Buzz Build') {
       steps {
 		sh './jenkins/build.sh'
-        archiveArtifacts(artifacts: 'target/*.jar', fingerprint: true)
+        archiveArtifacts(artifacts: 'target/*.jar', fingerprint: true) 
         }
     }
 
     stage('Buzz Test') {
       steps {
         sh './jenkins/test-all.sh'
-        junit '**/surefire-reports/**/*.xml'
+        junit '**/surefire-reports/**/*.xml' //Escriba la ruta de acceso de los archivos XML que contienen los informes de prueba en el cuadro "Resultados de la prueba". Para Apache Maven, la ruta es **/surefire-reports/**/*.xml.
        }
     }
   }
