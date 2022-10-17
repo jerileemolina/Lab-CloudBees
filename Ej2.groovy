3d7486846a307e18b876cb2c27c712fe0dee34e0
jerileemolina <j.molina.velasco@accenture.com>
2022-10-17 09:13:04 +0200

    Modificación dos

--- Ej2.groovy
+++ Ej2.groovy
@@ -11,24 +11,27 @@ pipeline {
       steps {
         echo 'Buzz,Bees,Buzz!'
         echo 'Bees Buzzing'
-       }
+    }
+}
 
     stage('Fluffy Build') {
       steps {
 		echo 'Placeholder'
         sh 'echo Edited Placeholder'
         }
+    }
 
     stage('Fluffy Test') {
       steps {
 	  sh 'sleep 5'
       echo 'Success'
         }
+    }
 
     stage('Fluffy Deploy Stage') {
       steps {
 	  echo 'Placeholder'
-      
+
         }
      }
    }
