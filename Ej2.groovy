6efe86492ac6d661fddc00eeea93ceffd9e5b7f8
jerileemolina <j.molina.velasco@accenture.com>
2022-10-17 09:09:28 +0200

    Modificado

--- Ej2.groovy
+++ Ej2.groovy
@@ -1,31 +1,34 @@
 pipeline {
-    agent any
-    stages { 
-        stage('Buzz Buzz') {
-            steps {
+  agent any
+  stages { 
+    stage('Buzz Buzz') {
+      steps {
 		echo 'Bees Buzz!'
-        echo 'Cambio main'
         }
     }
-        stage('Bees bees') {
-            steps {
+
+    stage('Bees bees') {
+      steps {
         echo 'Buzz,Bees,Buzz!'
         echo 'Bees Buzzing'
        }
-        stage('Fluffy Build') {
-            steps {
+
+    stage('Fluffy Build') {
+      steps {
 		echo 'Placeholder'
         sh 'echo Edited Placeholder'
         }
-         stage('Fluffy Test') {
-            steps {
-		sh 'sleep 5'
-        echo 'Success'
+
+    stage('Fluffy Test') {
+      steps {
+	  sh 'sleep 5'
+      echo 'Success'
         }
-         stage('Fluffy Deploy Stage') {
-            steps {
-		echo 'Placeholder'
-        echo 'Etapa final'
+
+    stage('Fluffy Deploy Stage') {
+      steps {
+	  echo 'Placeholder'
+      
         }
      }
    }
