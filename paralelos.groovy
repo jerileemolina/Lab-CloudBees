52ec7e65f71526f5db09bd04736f7792f0677e4c
jerileemolina <j.molina.velasco@accenture.com>
2022-10-18 09:55:34 +0200

    Ejemplo con paralelos

---
+++ paralelos.groovy
@@ -0,0 +1,39 @@
+pipeline {
+ agent any
+   stages {
+     stage (Primera etapa sin paralelo) {
+       steps {
+         echo ' Soy la primera etapa sin paralelo'
+       }
+     }
+
+    stage (Segunda etapa con paralelos dentro) {
+      parallel {
+        stage (Primer paso con paralelo) {
+          steps {
+          echo 'Soy la primera etapa con paralelo'
+            }
+        }
+         stage (Segundo paso con paralelo) {
+           steps {
+           echo 'Soy la segunda etapa con paralelo'
+           }
+        }
+
+         stage (Tercer paso con paralelo) {
+            steps {
+            echo 'Soy la tercera etapa con paralelo'
+            }
+         }
+
+        stage (Tercera etapa sin paralelos) {
+          steps {
+          echo 'Soy la ultima etapa sin paralelos'
+          }
+        }
+
+        }
+    }
+}
+
+}
